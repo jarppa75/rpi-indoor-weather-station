d6ed554d69bc45e9b36719994d0fb94d1faf98ad
Jonas Fredriksson <jonas_fredriksson@hotmail.com>
2016-10-09 18:18:56 +0200

    Inefficient solution to make sure the index exists

--- src/datastore.py
+++ src/datastore.py
@@ -17,6 +17,10 @@ class DataStore:
     def save_data(self, weatherdata):
         self.logger.debug('Save weather data to index')
         indexname = self.indexnamepattern + datetime.today().strftime('%Y.%m.%d')
+
+        if not self.elasticsearchclient.indices.exists(index=indexname):
+            self.elasticsearchclient.indices.create(index=indexname)
+
         self.elasticsearchclient.index(index=indexname,
                                         doc_type="weather",
                                         body={"temperature": weatherdata.temperature, "humidity": weatherdata.humidity, "barometricpressure": weatherdata.barometricpressure, "timestamp": int(time.time())})
